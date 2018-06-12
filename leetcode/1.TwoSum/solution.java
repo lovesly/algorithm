aea542af12c91306722c15a87a8d30fe0e687aad
Zihan Zhang <zzhlovesly@outlook.com>
2018-06-12 22:14:29 +0800

    leetcode again after a fucking year, problem 1 two sum

---
+++ leetcode/1.TwoSum/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < len; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
