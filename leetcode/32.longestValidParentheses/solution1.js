3641db91aea829b1891ba828a11bf7d0d78ee38c
ZZ <zhangzihan@didiglobal.com>
2020-01-21 22:48:40 +0800

    stack method

--- leetcode/32.longestValidParentheses/solution1.js
+++ leetcode/32.longestValidParentheses/solution1.js
@@ -7,6 +7,7 @@ var longestValidParentheses = function(s) {
   if (!s) return 0;
   const len = s.length;
   let max = 0;
+  // 这个是 base 坐标，如果中间有非法的括号，那么就从非法括号处开始计算。
   const st = [-1];
   for (let i = 0; i < len; i++) {
       if (s[i] === '(') {
@@ -14,6 +15,7 @@ var longestValidParentheses = function(s) {
       } else {
           st.pop();
           // ? 啥意思，先 pop，再 push？
+          // 如果中间有非法的括号，那么就从非法括号处开始计算。
           if (st.length === 0) {
               st.push(i);
           } else {
