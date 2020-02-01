79406fd06e90b5955fc3984aef87f3b1e3280cfa
ZZ <zhangzihan@didiglobal.com>
2020-02-01 22:09:56 +0800

    dp solution

--- leetcode/647.countSubstrings/solution3.js
+++ leetcode/647.countSubstrings/solution3.js
@@ -1,1 +1,34 @@
 // 马拉车算法
+// 有点酷炫，第一遍没看懂
+// 有点意思啊，但是题解里为什么要构建一个字符串呢，把空格用 # 替代这个我理解为用于兼容奇偶
+// 头尾的 @ 和 $ 是必须的吗？是为了计算凑数？
+/**
+     public int countSubstrings(String S) {
+        char[] A = new char[2 * S.length() + 3];
+        A[0] = '@';
+        A[1] = '#';
+        A[A.length - 1] = '$';
+        int t = 2;
+        for (char c: S.toCharArray()) {
+            A[t++] = c;
+            A[t++] = '#';
+        }
+
+        int[] Z = new int[A.length];
+        int center = 0, right = 0;
+        for (int i = 1; i < Z.length - 1; ++i) {
+            if (i < right)
+            // 什么时候会走到这里呢？
+                Z[i] = Math.min(right - i, Z[2 * center - i]);
+            while (A[i + Z[i] + 1] == A[i - Z[i] - 1])
+                Z[i]++;
+            if (i + Z[i] > right) {
+                center = i;
+                right = i + Z[i];
+            }
+        }
+        int ans = 0;
+        for (int v: Z) ans += (v + 1) / 2;
+        return ans;
+    }
+ */
